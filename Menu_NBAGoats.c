8672f7dea2d6083ec7ce486902db976c7e4b6a56
jorgegilp <37810877+jorgegilp@users.noreply.github.com>
2019-03-22 20:24:59 +0100

    Update Menu_NBAGoats.c
    Este es menu que queremos tener en el inicio del programa, aunque no es 100% seguro

--- Menu_NBAGoats.c
+++ Menu_NBAGoats.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-main()
-{
-int opcion;
-printf("\nNBA GOATS\n\n");
-printf("Bienvenido al programa, elige donde quieres continuar:\n\n\n");
-printf("1-Informacion acerca de los jugadores\n\n");
-printf("2-Rellena tu bracket\n\n");
-printf("3-Elige tus premios all star\n\n");
-scanf ("%d",&opcion);
 
-
-switch (opcion)
-{
-case 1:printf ("Adelante");
-break;
-case 2:printf ("Bienvenido");
-break;
-case 3:printf ("Saludos");
-break;
-default:
- printf ("Opcion incorrecta\n");
+int main(){
+	
+	int eleccion;
+	
+	do {
+	printf("Bienvenidos al mejor programa de la NBA \nNBA GOATs\n");
+	printf("1. Informacion acerca de los jugadores\n");
+	printf("2. Rellena tu bracket de los playoffs\n");
+	printf("3. Elige tus premios all star\n");
+	printf("4. Salir del programa\n");
+	
+	scanf("%d", &eleccion);
+	}while (eleccion > 4 || eleccion <=0);
+	
+	switch (eleccion){
+		case 1:
+				printf("Estos son los jugadores que quieres ver\n");
+			break;
+		
+		case 2:
+				printf("Estos son los playoffs\n");
+			break;
+		
+		case 3:
+			printf("Tu all star sera...\n");
+			break;
+		
+		case 4: 
+			printf("Adios\n");
+			break;
+		
+	}
 }
-
-}
-
